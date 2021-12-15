9fe88561062c79ff0b00f4a83c0485589ed7d13c
hadikp <hp20001025@gmail.com>
2021-12-15 19:17:09 +0100

    sentences

--- sv2-2021-jvjbf-masodik-reszvizsga-potvizsga/src/main/java/algorithms/Sentences.java
+++ sv2-2021-jvjbf-masodik-reszvizsga-potvizsga/src/main/java/algorithms/Sentences.java
@@ -5,7 +5,42 @@ import java.util.List;
 
 public class Sentences {
 
-    private List<String> list = new ArrayList<>();
+    private List<String> sentences = new ArrayList<>();
 
+    public void addSentence(String sentence) {
+        if (isUpperCaseString(sentence)) {
+            sentences.add(sentence);
+        } else {
+            throw new IllegalArgumentException("Sentence must start with capital!");
+        }
+        if (sentence.endsWith(".") || sentence.endsWith("?") || sentence.endsWith("!")) {
+            sentences.add(sentence);
+        } else {
+            throw new IllegalArgumentException("Sentence must end with ending mark!");
+        }
+    }
 
+    private boolean isUpperCaseString(String sentence) {
+        return Character.isUpperCase(sentence.charAt(0));
+    }
+
+    public String  findLongestSentence() {
+        int max = sentences.get(0).length();
+        String line = sentences.get(0);
+        for (String st: sentences) {
+            if (max < st.length()) {
+                max = st.length();
+                line = st;
+            }
+        }
+        if (line == null) {
+            throw new IllegalStateException("A lista nem lehet üres!");
+        }
+        return line;
+    }
+
+    public List<String> getSentences() {
+
+        return new ArrayList<>(sentences);
+    }
 }
